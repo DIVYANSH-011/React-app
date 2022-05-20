37fe9a0e86d4ee05e93b085493aff6b5ceed18dd
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-05-21 01:55:57 +0530

    commmit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ClassClick from "./components/ClassClick"
 import EventBind from "./components/EventBind"
 import ParentComponent from "./components/ParentComponent"
 import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,7 @@ function App() {
       <EventBind /> */}
       {/* <ParentComponent /> */}
       <UserGreeting />
+      <NameList />
 
       {/* <Counter />
       {/* <Message /> */}
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 function NameList() {
     const names = ['Divyansh', 'Clark' , 'Sonam']
+    const namelist = names.map(name => <h2>{name}</h2>)
     return (
-        <div>
-            <h2>{names[0]}</h2>
-            <h2>{names[1]}</h2>
-            <h2>{names[2]}</h2>
-        </div>
+        <div>{ namelist }</div>
     );
 }
 
