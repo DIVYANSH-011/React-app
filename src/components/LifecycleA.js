d242c720937a165e467ebe6f2be2a248d5e96ac4
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-05-31 01:24:19 +0530

    commit

---
+++ src/components/LifecycleA.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class LifecycleA extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            name: 'divyansh'
+
+        }
+    }    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LifecycleA;
