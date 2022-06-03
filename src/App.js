b84213421624edeb779d4967041376ed98859a5b
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-03 23:30:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -18,28 +18,7 @@ import LifecycleA from './components/LifecycleA';
 function App() {
   return (
     <div className="App">
-      {/* <FunctionClick />
-      <ClassClick />
-      <EventBind /> */}
-      {/* <ParentComponent /> */}
-      {/* <UserGreeting />
-      <NameList />
-      <StyleSheet Primary={true} />
-      <Inline /> */}
-      {/* <Form /> */}
-      <LifecycleA />
-
-      {/* <Counter />
-      {/* <Message /> */}
-       {/* <Greet name = 'lamba'heroname = "paddu" >
-        <p>This is children prop</p>
-      </Greet>
-      <Greet name = 'divyansh'heroname = "captain america" >
-        <button>Action</button>
-      </Greet>  */}
-      {/* <Greet name = 'who care'heroname = "who cares"  />
-      <Welcome name = "divyansh" heroname = "singh "> </Welcome>
-      <Hello />  */}
+     
     </div>
   );
 }
