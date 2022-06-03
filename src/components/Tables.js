ed1cfbbb08371579ccf62129b39261f9332bfb65
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-03 23:41:57 +0530

    commit

---
+++ src/components/Tables.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Tables(props) {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default Tables;
