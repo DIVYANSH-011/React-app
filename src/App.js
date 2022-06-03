afed1de1851e9058a5ac3552dfbb6cc7e196ebb5
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-03 23:31:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ import StyleSheet from './components/StyleSheet';
 import Inline from './components/Inline';
 import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
 function App() {
   return (
     <div className="App">
+      <FragementDemo />
      
     </div>
   );
