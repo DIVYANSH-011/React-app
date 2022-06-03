cce5b6c1c9945c00c3242f8ce4b6a760aa7606ac
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-03 23:38:01 +0530

    commit

--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 function FragmentDemo(props) {
     return (
-        <div>
+        <React.Fragment>
             <h1>
                 Fragmment Demo
             </h1>
             <p> This describes the demo</p>
-        </div>
+        </React.Fragment>
     );
 }
 
