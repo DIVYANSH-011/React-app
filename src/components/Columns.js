76483ad6d203b2315ff7817d8789960386cfc026
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-03 23:43:13 +0530

    commit

---
+++ src/components/Columns.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Columns(props) {
+    return (
+        <div>
+            <td>Name</td>
+            <td>Divyansh</td>
+            
+        </div>
+    );
+}
+
+export default Columns;
