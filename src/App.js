083ed1271fd57865cdf587ee2cad8c6e508059af
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-05 02:05:24 +0530

    ommti

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/welcome'
-import Hello from './components/Hello'
-import Message from "./components/message"
-import Counter from "./components/counter"
-import FunctionClick from "./components/FunctionClick"
-import ClassClick from "./components/ClassClick"
-import EventBind from "./components/EventBind"
-import ParentComponent from "./components/ParentComponent"
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Tables from './components/Tables';
-import ParentComponent from './components/ParentComponent';
-import PureComponent from './components/PureComponent';
-function App() {
-  return (
-    <div className="App">
-      <PureComponent />
+import React, { Component } from 'react';
+import './App.css'; 
+import PureComp  from './components/PureComp';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PureComp />
+        
       
-     
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
