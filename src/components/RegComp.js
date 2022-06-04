a230e8a6d13660c0f84c9ade3cc5060410abf705
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-05 02:07:05 +0530

    commit

---
+++ src/components/RegComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RegComp extends Component {
+    render() {
+        return (
+            <div>
+                regular component
+            </div>
+        );
+    }
+}
+
+export default RegComp;
