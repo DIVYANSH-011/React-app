6855845008ba80373f2128090123f36e98355fdf
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-05 02:10:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css'; 
 import PureComp  from './components/PureComp';
+import RegComp from './components/RegComp';
+import ParentComp from './components/ParentComp';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <PureComp />
+        <RegComp />
+        <ParentComp />
         
       
       </div>
