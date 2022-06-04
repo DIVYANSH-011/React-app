4239c58918a209cd749d6bde40b9986eeb826972
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-05 02:19:45 +0530

    commit

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import RegComp from './RegComp';
 
 class ParentComp extends Component {
     
     constructor(props) {
         super(props)
-        
+
         this.state = {
-            name: 'Parent'
+            name: 'Divyansh'
         }
     }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: 'Divyansh'
+            })
+        },2000)
+    }
+        
     render() {
         return (
             <div>
                 ParentComp
+                <RegComp name = {this.state.name} />
+                <Purecomp name = {this.state.name} />
+
             </div>
         );
     }
