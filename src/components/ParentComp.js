08260903ffdfe122ca34a7d3460bcbca03f9e974
DIVYANSH-011 <singhdivyansh441@gmail.com>
2022-06-05 02:16:17 +0530

    commit

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class ParentComp extends Component {
+    
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            name: 'Parent'
+        }
+    }
     render() {
         return (
             <div>
